a6481043fe3fab46f2d111f7b9c3c3d1f9e89885
Thiago S Shimada Ramos <shimada@nikkei.one>
2025-02-03 04:22:45 +1100

    migrating to sqlite

--- src/questions.js
+++ src/questions.js
@@ -1,12 +1,12 @@
 // questions.js
 
 // Import domain-specific questions
-import { cloudNativeSecurityQuestions } from './questions-per-domain/cloudNativeSecurityQuestions';
-import { kubernetesClusterComponentSecurityQuestions } from './questions-per-domain/kubernetesClusterComponentSecurityQuestions';
-import { kubernetesSecurityFundamentalsQuestions } from './questions-per-domain/kubernetesSecurityFundamentalsQuestions';
-import { kubernetesThreatModelQuestions } from './questions-per-domain/kubernetesThreatModelQuestions';
-import { platformSecurityQuestions } from './questions-per-domain/platformSecurityQuestions';
-import { complianceAndSecurityFrameworksQuestions } from './questions-per-domain/complianceAndSecurityFrameworksQuestions';
+import { cloudNativeSecurityQuestions } from './questions-per-domain/cloudNativeSecurityQuestions.js';
+import { kubernetesClusterComponentSecurityQuestions } from './questions-per-domain/kubernetesClusterComponentSecurityQuestions.js';
+import { kubernetesSecurityFundamentalsQuestions } from './questions-per-domain/kubernetesSecurityFundamentalsQuestions.js';
+import { kubernetesThreatModelQuestions } from './questions-per-domain/kubernetesThreatModelQuestions.js';
+import { platformSecurityQuestions } from './questions-per-domain/platformSecurityQuestions.js';
+import { complianceAndSecurityFrameworksQuestions } from './questions-per-domain/complianceAndSecurityFrameworksQuestions.js';
 
 // Merge all arrays into one
 export const questions = [
