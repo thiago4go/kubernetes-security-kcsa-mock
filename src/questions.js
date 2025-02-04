9fbe713af69a16c37990eac315ced443443474fb
Thiago S Shimada Ramos <shimada+github@nikkei.one>
2025-02-04 15:32:34 +1100

    fix tests

--- src/questions.js
+++ src/questions.js
@@ -1,12 +1,12 @@
 // questions.js
 
 // Import domain-specific exported questions
-import { CloudNativeSecurityQuestions } from './exported-questions/Cloud_Native_Security.js';
-import { KubernetesClusterComponentSecurityQuestions } from './exported-questions/Kubernetes_Cluster_Component_Security.js';
-import { KubernetesSecurityFundamentalsQuestions } from './exported-questions/Kubernetes_Security_Fundamentals.js';
-import { KubernetesThreatModelQuestions } from './exported-questions/Kubernetes_Threat_Model.js';
-import { PlatformSecurityQuestions } from './exported-questions/Platform_Security.js';
-import { ComplianceAndSecurityFrameworksQuestions } from './exported-questions/Compliance_and_Security_Frameworks.js';
+import { CloudNativeSecurityQuestions } from './exported-questions/Cloud_Native_Security.mjs';
+import { KubernetesClusterComponentSecurityQuestions } from './exported-questions/Kubernetes_Cluster_Component_Security.mjs';
+import { KubernetesSecurityFundamentalsQuestions } from './exported-questions/Kubernetes_Security_Fundamentals.mjs';
+import { KubernetesThreatModelQuestions } from './exported-questions/Kubernetes_Threat_Model.mjs';
+import { PlatformSecurityQuestions } from './exported-questions/Platform_Security.mjs';
+import { ComplianceAndSecurityFrameworksQuestions } from './exported-questions/Compliance_and_Security_Frameworks.mjs';
 
 // Merge all arrays into one
 export const questions = [
